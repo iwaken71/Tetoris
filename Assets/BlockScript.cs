5642170dd206b28fcf843a287492594302e005b2
iwaken71 <iwaken59371518@gmail.com>
2016-06-22 04:05:48 +0900

    a

---
+++ Assets/BlockScript.cs
@@ -0,0 +1,167 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockScript : MonoBehaviour {
+
+	bool move,left,right;
+	GameObject[] child;
+	public int id;
+
+	// Use this for initialization
+	void Start () {
+		move = true;
+		left = true;
+		right = true;
+		child = new GameObject[transform.childCount];
+		for (int i = 0; i < transform.childCount; i++) {
+			child [i] = transform.GetChild (i).gameObject;
+		}
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		LeftCheck ();
+		RightCheck ();
+		DownCheck ();
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			if (move) {
+				transform.position += Vector3.back;
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			if (move && left) {
+				transform.position += Vector3.left;
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			if (move && right) {
+				transform.position += Vector3.right;
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.X)) {
+			if (move) {
+				Vector3 tmp1 = transform.position;
+				Quaternion tmp2 = transform.rotation;
+				transform.Rotate (new Vector3(0,90,0));
+				Fix ();
+				bool check = true;
+				for (int i = 0; i < transform.childCount; i++) {
+					Vector3 vec = child [i].transform.position;
+					if (GameManager.instance.isBlock (vec.x, vec.z)) {
+						check = false;
+						break;
+					}
+				}
+				if (!check) {
+					transform.position = tmp1;
+					transform.rotation = tmp2;
+				}
+			}
+		}
+	}
+
+	void LeftCheck(){
+
+		bool check = true;
+		for (int i = 0; i < transform.childCount; i++) {
+			Ray ray = new Ray (child [i].transform.position, Vector3.left);
+			RaycastHit hit;
+			float distance = child [i].transform.localScale.x / 2;
+			if (Physics.Raycast (ray, out hit, distance)) {
+				if (hit.collider.tag == "Block") {
+					int tmp = hit.collider.GetComponent<BlockScript> ().id;
+					if (id != tmp) {
+						check = false;
+						break;
+					}
+				} else {
+					check = false;
+					break;
+				}
+			}
+		}
+		left = check;
+	}
+	void RightCheck(){
+
+		bool check = true;
+		for (int i = 0; i < transform.childCount; i++) {
+			Ray ray = new Ray (child [i].transform.position, Vector3.right);
+			RaycastHit hit;
+			float distance = child [i].transform.localScale.x / 2;
+			if (Physics.Raycast (ray, out hit, distance)) {
+				if (hit.collider.tag == "Block") {
+					int tmp = hit.collider.GetComponent<BlockScript> ().id;
+					if (id != tmp) {
+						check = false;
+						break;
+					}
+				} else {
+					check = false;
+					break;
+				}
+			}
+		}
+		right = check;
+	}
+
+	void Fix(){
+		for (int i = 0; i < transform.childCount; i++) {
+			if (child [i].transform.position.x >= 10) {
+				transform.position += Vector3.left;
+				Fix ();
+			}
+		}
+		for (int i = 0; i < transform.childCount; i++) {
+			if (child [i].transform.position.x <= -1) {
+				transform.position += Vector3.right;
+				Fix ();
+			}
+		}
+
+	}
+
+	void DownCheck(){
+		if (move) {
+			bool check = true;
+			for (int i = 0; i < transform.childCount; i++) {
+				Ray ray = new Ray (child [i].transform.position, Vector3.back);
+				RaycastHit hit;
+				float distance = child [i].transform.localScale.z / 2;
+				if (Physics.Raycast (ray, out hit, distance)) {
+					if (hit.collider.tag == "Block") {
+						int tmp = hit.collider.GetComponent<BlockScript> ().id;
+						if (id != tmp) {
+							check = false;
+							break;
+
+						}
+					} else {
+						check = false;
+						break;
+					}
+				}
+			}
+			move = check;
+			if (!move) {
+				GameManager.instance.NewBlock ();
+				for (int i = 0; i < transform.childCount; i++) {
+					Vector3 vec = child [i].transform.position;
+
+					GameManager.instance.SetBlock (vec.x,vec.z);
+					Destroy (this.gameObject);
+				}
+
+			}
+		}
+
+	}
+
+	public void SetId(int input){
+		id = input;
+	}
+
+
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+
+	public GameObject block;
+	int number = 0;
+	public int width,height;
+	public GameObject oneBlock;
+
+	int[,] board; // 情報 
+
+	GameObject[,] cube; 
+
+	void Awake(){
+		if (instance == null) {
+			instance = this;
+			DontDestroyOnLoad (this.gameObject);
+
+		} else {
+			Destroy (this.gameObject);
+		}
+
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		width = 10;
+		height = 20;
+		BoardStart ();
+		NewBlock ();
+	}
+
+	void BoardStart(){
+		board = new int[width,height];
+		cube = new GameObject[width, height];
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				board [i, j] = 0;
+				cube [i, j] = Instantiate(oneBlock,new Vector3 (i,0,j),Quaternion.identity)as GameObject;
+
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Draw ();
+	}
+
+	void Draw(){
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				if (board [i, j] >= 1) {
+					cube [i, j].SetActive (true);
+				} else {
+					cube [i, j].SetActive (false);
+				}
+
+			}
+		}
+	}
+
+	public void NewBlock(){
+		GameObject obj = Instantiate (block, new Vector3 (5, 0, height-4), Quaternion.identity)as GameObject;
+		obj.GetComponent<BlockScript> ().SetId (number);
+		number++;
+	}
+
+	public void SetBlock(float x,float z){
+		int i = (int)(x+0.1f);
+		int j = (int)(z+0.1f);
+		board [i, j] = 1;
+	}
+
+	public bool isBlock(float x,float z){
+		int i = (int)(x+0.1f);
+		int j = (int)(z+0.1f);
+		return board [i, j] >= 1;
+
+	}
+	void DeleteCheck(){
+		for (int j = 0; j < height; j++) {
+			bool ichiretu = false;
+			for (int i = 0; i < width; i++) {
+				if (board [i, j] >= 1) {
+
+				} else {
+					ichiretu = false;
+				}
+					
+
+			}
+			if (ichiretu) {
+				for (int i = 0; i < width; i++) {
+					board [i, j] = 0;
+				}
+			}
+		}
+	}
+}
